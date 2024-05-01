f18c4f648a82beda85ea49dc9f3cf2e7694a8f00
mush42 <musharraf.omer@gmail.com>
2024-05-01 22:05:59 +0200

    Updated version

--- libtashkeel-wasm/main.ts
+++
@@ -1,6 +0,0 @@
-import { do_tashkeel, instantiate } from "./lib/libtashkeel_wasm.generated.js";
-
-if (import.meta.main) {
-  await instantiate();
-  console.log(do_tashkeel("بسم الله الرحمن الرحيم"));
-}
